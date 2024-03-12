b1da9a23478d514726519889553b0c4e0d36d273
Chavelle Kassie <ckimberley.kassie@gmail.com>
2024-03-12 00:15:13 -0500

    First commits.

---
+++ script.js
@@ -0,0 +1,19 @@
+const plus = document.querySelectorAll(".plus");
+const minus = document.querySelectorAll(".minus");
+const panel = document.querySelectorAll(".panel");
+
+for(let i = 0; i < plus.length; i++) {
+    plus[i].addEventListener('click', function() {
+        plus[i].classList.toggle("hidden");
+        minus[i].classList.toggle("hidden");
+        panel[i].classList.toggle("hidden");
+    });
+}
+
+for(let i = 0; i < minus.length; i++) {
+    minus[i].addEventListener('click', function() {
+        plus[i].classList.toggle("hidden");
+        minus[i].classList.toggle("hidden");
+        panel[i].classList.toggle("hidden");
+    });
+}
